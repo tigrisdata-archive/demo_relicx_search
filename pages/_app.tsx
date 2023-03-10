fea400b4b62139a656cedfe76d665a0e092556cc
Taha Khan <tkhan@tigrisdata.com>
2023-03-09 16:23:46 -0800

    Adding Error boundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import '@tremor/react/dist/esm/tremor.css';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />;
+    </ErrorBoundary>
+  );
 }
 
 export default MyApp;
